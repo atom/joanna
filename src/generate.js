caed9837986fda86db99430e859b760edb1272de
Max Brunsfeld <maxbrunsfeld@gmail.com>
2016-08-01 11:23:35 -0700

    Group adjacent comments, record section dividers

--- src/generate.js
+++ src/generate.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const assert = require('assert')
 const babylon = require('babylon')
 
 module.exports = function generate (filename, code) {
@@ -8,6 +7,7 @@ module.exports = function generate (filename, code) {
 }
 
 const BABYLON_OPTIONS = {sourceType: 'module'}
+const API_STATUS_REGEX = /^(Private|Essential|Extended|Section):/
 
 class Generator {
   constructor (filename, code) {
@@ -27,20 +27,20 @@ class Generator {
       case 'FunctionDeclaration': return this.visitFunctionDeclaration(node)
       case 'ClassDeclaration': return this.visitClassDeclaration(node)
       case 'ClassMethod': return this.visitMethodDefinition(node)
-      default:                 return this.visitNodeWithChildren(node)
+      default: return this.visitNodeWithChildren(node)
     }
   }
 
   visitFunctionDeclaration (node) {
-    this.addObject(node, {
+    this.addObject(node.loc, {
       type: 'function',
       name: node.id.name,
-      bindingType: 'exportsProperty',
+      bindingType: 'exportsProperty'
     })
   }
 
   visitClassDeclaration (node) {
-    this.classStack.push(this.addObject(node, {
+    this.classStack.push(this.addObject(node.loc, {
       type: 'class',
       name: node.id.name,
       superClass: node.superClass && node.superClass.name,
@@ -54,8 +54,8 @@ class Generator {
   }
 
   visitMethodDefinition (node) {
-    const currentClass = top(this.classStack)
-    const currentMethod = this.addObject(node, {
+    const currentClass = last(this.classStack)
+    const currentMethod = this.addObject(node.loc, {
       type: 'function',
       name: node.key.name,
       doc: this.getDocumentation(node),
@@ -88,26 +88,59 @@ class Generator {
   }
 
   getDocumentation (node) {
-    if (node.leadingComments) {
-      const lines = node.leadingComments.map(commentNode => commentNode.value)
-      return 'Private:' + lines.join('\n') + ' '
+    if (!node.leadingComments) {
+      return
     }
+
+    let lastComment = null
+    let groupedComments = []
+    for (const comment of node.leadingComments) {
+      if (lastComment && lastComment.loc.end.line === comment.loc.start.line - 1) {
+        lastComment.value += '\n' + comment.value.trim()
+        lastComment.loc.end = comment.loc.end
+      } else {
+        lastComment = {
+          value: comment.value.trim(),
+          loc: comment.loc
+        }
+        groupedComments.push(lastComment)
+      }
+    }
+
+    // Add any previous comments as separate top-level items
+    groupedComments.pop()
+    for (const comment of groupedComments) {
+      this.addObject(comment.loc, {
+        type: 'comment',
+        doc: comment.value
+      })
+    }
+
+    return ensureAPIStatusTag(lastComment.value)
   }
 
-  addObject (node, object) {
-    const line = node.loc.start.line - 1
-    const column = node.loc.start.column
-    const endLine = node.loc.end.line - 1
-    const endColumn = node.loc.end.column
+  addObject (location, object) {
+    const line = location.start.line - 1
+    const column = location.start.column
+    const endLine = location.end.line - 1
+    const endColumn = location.end.column
     if (!this.result[line]) {
       this.result[line] = {}
     }
-    return this.result[line][column] = Object.assign(object, {
+    return (this.result[line][column] = Object.assign(object, {
       range: [[line, column], [endLine, endColumn]]
-    })
+    }))
   }
 }
 
-function top (stack) {
+function ensureAPIStatusTag (text) {
+  if (API_STATUS_REGEX.test(text)) {
+    return text
+  } else {
+    return 'Private: ' + text
+  }
+}
+
+function last (stack) {
   return stack[stack.length - 1]
 }
--- test/generate-test.js
+++ test/generate-test.js
@@ -41,13 +41,13 @@ describe('generate(code)', function () {
       type: 'class',
       name: 'Person',
       superClass: 'Animal',
-      doc: 'Private: A person class ',
+      doc: 'Private: A person class',
       range: [[1, 0], [11, 1]],
       bindingType: undefined,
       classProperties: [],
       prototypeProperties: [
         [2, 2],
-        [8, 2],
+        [8, 2]
       ]
     })
 
@@ -63,11 +63,59 @@ describe('generate(code)', function () {
     assert.deepEqual(result['8']['2'], {
       type: 'function',
       name: 'getName',
-      doc: 'Private: Get the name ',
+      doc: 'Private: Get the name',
       range: [[8, 2], [10, 3]],
       bindingType: 'prototypeProperty',
       paramNames: []
     })
+  })
+
+  it('handles section divider comments', function () {
+    const donnaResult = runDonna('test.coffee', dedent`
+      # A useful class
+      class Person
+        constructor: (name) ->
+          @name = name
+
+        ###
+        Section: Getters
+        ###
+
+        ###
+        Something else
+        ###
+
+        # Essential: Get the name
+        # Does stuff
+        # Returns a string.
+        getName: -> @name
+    `)
+
+    const result = generate('test.js', dedent`
+      // A useful class
+      class Person {
+        constructor (name) {
+          this.name = name
+        }
+
+        /*
+        Section: Getters
+        */
+
+        /*
+        Something else
+        */
+
+        // Essential: Get the name
+        // Does stuff
+        // Returns a string.
+        getName () { return this.name }
+      }
+    `)
+
+    assertEquivalentMetadata(result, donnaResult, [1, 0], [1, 0])
+    assertEquivalentMetadata(result, donnaResult, [6, 2], [5, 2])
+    assertEquivalentMetadata(result, donnaResult, [10, 2], [9, 2])
   })
 
   it('handles top-level functions', function () {
@@ -114,37 +162,45 @@ describe('generate(code)', function () {
 })
 
 function assertEquivalentMetadata (actualObjects, expectedObjects, actualPosition, expectedPosition) {
-  assertMatch(actualPosition, expectedPosition, [])
+  assertMatchingObject(actualPosition, expectedPosition, [])
 
-  function assertMatch (actualPos, expectedPos, keyPath) {
+  function assertMatchingObject (actualPos, expectedPos, keyPath) {
     const actualObject = actualObjects[actualPos[0]][actualPos[1]]
     const expectedObject = expectedObjects[expectedPos[0]][expectedPos[1]]
 
-    assert(expectedObject, "No expected object at the given position")
-    assert(actualObject, "No actual object at the given position")
+    assert(expectedObject, 'No expected object at the given position')
+    assert(actualObject, 'No actual object at the given position')
 
     const expectedKeys = Object.keys(expectedObject).sort()
     assert.deepEqual(Object.keys(actualObject).sort(), expectedKeys)
 
-    for (let key of expectedKeys)
-      if (key !== 'range')
+    for (let key of expectedKeys) {
+      if (key === 'range') {
+        continue
+      } else if (key === 'doc') {
+        const actualDoc = actualObject.doc && actualObject.doc.trim()
+        const expectedDoc = expectedObject.doc && expectedObject.doc.trim()
+        assertMatchingProperty(actualDoc, expectedDoc, keyPath.concat([key]))
+      } else {
         assertMatchingProperty(actualObject[key], expectedObject[key], keyPath.concat([key]))
+      }
+    }
   }
 
   function assertMatchingProperty (actual, expected, keyPath) {
     if (Array.isArray(expected)) {
-      assert.equal(actual.length, expected.length, "Key path: " + keyPath.join('.'))
+      assert.equal(actual.length, expected.length, 'Length of key path: ' + keyPath.join('.'))
       for (let i = 0; i < expected.length; i++) {
         let expectedElement = expected[i]
         let actualElement = actual[i]
         if (isPosition(expectedElement)) {
-          assertMatch(actualElement, expectedElement, keyPath.concat([i]))
+          assertMatchingObject(actualElement, expectedElement, keyPath.concat([i]))
         } else {
-          assert.equal(actualElement, expectedElement, "Key path: " + keyPath.join('.') + i)
+          assert.equal(actualElement, expectedElement, 'Key path: ' + keyPath.join('.') + i)
         }
       }
     } else {
-      assert.equal(actual, expected, "Key path: " + keyPath.join('.'))
+      assert.equal(actual, expected, 'Key path: ' + keyPath.join('.'))
     }
   }
 }
