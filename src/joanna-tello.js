f2c5e10acb037eacd04f266474dcfcd7b3378c4c
Max Brunsfeld <maxbrunsfeld@gmail.com>
2018-11-14 14:33:28 -0800

    Make joanna-tello easier to use in a cross-platform way
    * Allow directories to be passed as source paths
    * Allow an output file path to be passed
    
    Refs https://github.com/atom/event-kit/pull/47

--- src/joanna-tello.js
+++ src/joanna-tello.js
@@ -1,27 +1,53 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-const tello = require('tello');
-const joanna = require('./index');
-const fs = require('fs');
+const fs = require('fs')
+const path = require('path')
+const tello = require('tello')
+const joanna = require('./index')
 
-// Generates API documentation in 'tello' format to stdout
-// Usage: joanna-tello <package.json> <js-files>
+const argv = require('yargs-parser')(process.argv.slice(2))
+const pathArgs = argv._
+const packageJsonPath = pathArgs.shift()
+const outputPath = argv.o || argv.output
 
-const args = process.argv.slice(2);
-const packageJsonPath = args[0];
-args.shift();
-const jsFiles = args;
+if (argv.help || argv.h || !packageJsonPath || pathArgs.length === 0) {
+  console.log(
+    'Usage: joanna-tello [-o <output-file>] <package-json-path> <source-path>...'
+  )
+  process.exit(0)
+}
 
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-const jsMetadata = joanna(jsFiles);
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'))
+
+// The source paths can be directories, in which case we
+// generate docs for all of the `.js` files in the directory.
+const jsFiles = []
+for (const pathArg of pathArgs) {
+  if (fs.statSync(pathArg).isDirectory()) {
+    for (const entry of fs.readdirSync(pathArg)) {
+      if (entry.endsWith('.js')) {
+        jsFiles.push(path.join(pathArg, entry))
+      }
+    }
+  } else {
+    jsFiles.push(pathArg)
+  }
+}
+
+const jsMetadata = joanna(jsFiles)
 const metadata = {
   repository: packageJson.repository.url,
   version: packageJson.version,
   files: jsMetadata.files
-};
+}
 
-const api = tello.digest([metadata]);
+const api = tello.digest([metadata])
+const output = JSON.stringify(api, null, 2)
 
-console.log(JSON.stringify(api, null, 2));
+if (outputPath) {
+  fs.writeFileSync(outputPath, output)
+} else {
+  console.log(output)
+}
